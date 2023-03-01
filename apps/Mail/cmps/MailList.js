52f45b3466edf23d392f85d81046a34ee4742759
Inon <inon.f4@gmail.com>
2023-03-01 18:27:40 +0200

    update mail preview

--- apps/Mail/cmps/MailList.js
+++ apps/Mail/cmps/MailList.js
@@ -1,25 +1,21 @@
 import { mailService } from '../services/mail.service.js'
+import MailPreview from '../cmps/MailPreview.js'
+
 export default {
     template: `
         <section class="email-box grid"> 
             <div class="toolbar flex">
                 <ul>
-	            <li><a href="#">All  </a></li>
+	            <li><a href="#">All</a></li>
 	            <li><a href="#">Inbox</a></li>
-	            <li><a href="#">Send </a></li>
+	            <li><a href="#">Send</a></li>
 	            <li><a href="#">Draft</a></li>
                 </ul>
             </div>
                <table class="mails-table">
                     <thead><td>From</td><td>To</td><td>Title</td><td>Date</td></thead>
-                    <tbody>
-                        <tr v-for="mail in mails" :key="mail.from"> 
-                            <td>{{ mail.from }}</td>
-                            <td>{{ mail.to }}</td>
-                            <td>{{ mail.body }}</td>
-                            <td>{{ mail.sentAt }}</td>
-                            <!-- <td><button @click="deleteReview(review.name)">X</button></td> -->
-                        </tr> 
+                    <tbody v-for="mail in mails" :key="mail.from">
+                        <MailPreview :mail="mail" @click="openMail(mail.id)"/>
                     </tbody>
                 </table>
         </section>
@@ -36,7 +32,13 @@ export default {
         updateMails() {
             mailService.query()
                 .then(mails => this.mails = mails)
+        },
+        openMail(id) {
+            console.log('id', id)
         }
     },
+    components: {
+        MailPreview,
+    }
 }
 
--- apps/Mail/cmps/MailPreview.js
+++ apps/Mail/cmps/MailPreview.js
@@ -1,10 +1,11 @@
-// export default {
-//     props: ['mail'],
-//     template: `
-//         <article class="mail-preview">
-//             <img :src=mail.thumbnail >
-//             <h3>{{ mail.title }}</h3>
-//             <h5>Price: {{ mail.listPrice.amount }} {{ this.mail.listPrice.currencyCode }} </h5>
-//         </article>
-//     `,
-// }
+export default {
+    props: ['mail'],
+    template: `
+         <tr> 
+            <td>{{ mail.from }}</td>
+            <td>{{ mail.to }}</td>
+            <td>{{ mail.body }}</td>
+            <td>{{ mail.sentAt }}</td>
+         </tr> 
+    `,
+}
